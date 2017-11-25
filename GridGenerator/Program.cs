df7b6f4723aaa4f80d05ff29c96fbc24b6ef741e
Maylon Pedroso <maylonpedroso@gmail.com>
2017-11-24 23:47:14 -0500

    Receive output csv file path as argument

--- GridGenerator/Program.cs
+++ GridGenerator/Program.cs
@@ -1,19 +1,20 @@
 ﻿using System;
+using System.IO;
 using System.Linq;
 
 namespace GridGenerator
 {
     internal class Program
     {
-        private const string USAGE = "GridGenerator.exe size";
+        private const string USAGE = "GridGenerator.exe size path/to/file.csv";
 
         /// <summary>
         /// Generate a grid (comma-separated) with the specified size and random values 
         /// </summary>
-        /// <param name="args">single element array containing size of grid</param>
+        /// <param name="args">two elements array containing size of grid and output file path</param>
         public static void Main(string[] args)
         {
-            if (args.Length != 1)
+            if (args.Length != 2)
             {
                 Console.Error.WriteLine(USAGE);
                 return;
@@ -26,11 +27,14 @@ namespace GridGenerator
                 return;
             }
 
+            var file = new StreamWriter(args[1]);
+
             var random = new Random();
             for (var i = 0; i < size; i++)
             {
-                Console.WriteLine(string.Join(",", Enumerable.Range(0, size).Select(r => random.Next(256)+1).ToArray()));                
+                file.WriteLine(string.Join(",", Enumerable.Range(0, size).Select(r => random.Next(256)+1).ToArray()));                
             }
+            file.Close();
         }
     }
 }
