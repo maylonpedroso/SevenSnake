232db63a581d70a7cef785da1913ec4b7263131a
Maylon Pedroso <maylonpedroso@gmail.com>
2017-11-24 21:28:03 -0500

    Create console application project to generate a random grid with specified size.

---
+++ GridGenerator/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace GridGenerator
+{
+    internal class Program
+    {
+        private const string USAGE = "GridGenerator.exe size";
+
+        /// <summary>
+        /// Generate a grid (comma-separated) with the specified size and random values 
+        /// </summary>
+        /// <param name="args">single element array containing size of grid</param>
+        public static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine(USAGE);
+                return;
+            }
+
+            int size;
+            if (!int.TryParse(args[0], out size))
+            {
+                Console.WriteLine("Invalid size number");
+                return;
+            }
+
+            var random = new Random();
+            for (var i = 0; i < size; i++)
+            {
+                Console.WriteLine(string.Join(",", Enumerable.Range(0, size).Select(r => random.Next(256)+1).ToArray()));                
+            }
+        }
+    }
+}
